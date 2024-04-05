a4674ecaf8bc13c55c6b9e65c166ec0a750e9d48
Dhanush <dhanush@Dhanush-4.local>
2024-04-05 17:15:18 +0530

    databse setup

--- internal/services/service.go
+++ internal/services/service.go
@@ -4,20 +4,34 @@ import (
 	"Cloudbee/github.com/dhanushcrueiso/blog/protos"
 	"context"
 	"log"
+	"sync"
 )
 
 type PostServiceServer struct {
 	protos.UnimplementedPostServiceServer
+	posts map[string]*protos.Post
+	mu    sync.Mutex
 }
 
+// NewPostService creates a new instance of PostService
+func NewPostService() *PostServiceServer {
+	return &PostServiceServer{
+		posts: make(map[string]*protos.Post),
+	}
+}
 func (s *PostServiceServer) CreatePost(ctx context.Context, req *protos.CreatePostRequest) (*protos.CreatePostResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	// In a real implementation, you'd likely store the post in a database
-	// For simplicity, let's just echo back the received post
+	// For now, let's store it in memory
 	post := req.GetPost()
 	log.Printf("Received request to create post: %+v", post)
 
 	// Simulate generating a unique PostID (in real world, use UUID or similar)
 	post.PostId = "123456789"
+
+	s.posts[post.PostId] = post
 
 	return &protos.CreatePostResponse{Post: post}, nil
 }
--- main.go
+++ main.go
@@ -3,24 +3,46 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"Cloudbee/github.com/dhanushcrueiso/blog/protos"
-	server "Cloudbee/internal"
+	service "Cloudbee/internal/services"
+
+	"Cloudbee/config"
+	db "Cloudbee/internal/database"
 
 	"google.golang.org/grpc"
 )
 
 func main() {
-	// Create a listener on TCP port 50051
+	// Create a listener on TCP port 50051env := "dev"
+	env := "dev"
+	var file *os.File
+	var err error
+
+	file, err = os.Open(env + ".json")
+	if err != nil {
+		log.Println("Unable to open file. Err:", err)
+		os.Exit(1)
+	}
+	//parsing json with the config and passing the dev.json values from here
+	var cnf *config.Config
+	config.ParseJSON(file, &cnf)
+	config.Set(cnf)
+
+	db.Init(&db.Config{
+		URL:       cnf.DatabaseURL,
+		MaxDBConn: cnf.MaxDBConn,
+	})
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	// Create a new gRPC server
 	grpcServer := grpc.NewServer()
-
-	// Register the PostServiceServer with the gRPC server
-	protos.RegisterPostServiceServer(grpcServer, &server.PostServiceServer{})
+	postService := service.NewPostService()
+	protos.RegisterPostServiceServer(grpcServer, postService)
 
 	log.Println("gRPC server listening on port 50051")
 	// Start the server
