0d10287529525a6331fffdd4dddc193874a9365f
Dhanush <dhanush@Dhanush-4.local>
2024-04-07 12:36:23 +0530

    working code through postman

--- internal/database/daos/post.go
+++ internal/database/daos/post.go
@@ -2,15 +2,32 @@ package daos
 
 import (
 	db "Cloudbee/internal/database"
+	"fmt"
+	"log"
+
 	"Cloudbee/internal/database/models"
+
+	"github.com/google/uuid"
 )
 
 func SaveCustomer(req *models.Post) error {
+	if req != nil {
+		if req.Author == "" || req.Content == "" || req.Id == uuid.Nil || req.Title == "" {
+			fmt.Println("first case")
+		}
+		if len(req.Tags) > 0 {
+			fmt.Println("tags")
+		}
 
-	err := db.DB.Debug().Unscoped().Table("posts").Create(&req).Error
+	}
+	log.Println("check 6")
+	err := db.DB.Debug().Unscoped().Table("posts").Create(req).Error
 	if err != nil {
+		fmt.Println("err", err)
+		log.Println(err)
 		return err
 	}
+	log.Println("check 3")
 	return err
 }
 
@@ -25,9 +42,35 @@ func GetAllPosts() ([]*models.Post, error) {
 
 func GetPost(id string) (*models.Post, error) {
 	res := models.Post{}
-	err := db.DB.Debug().Unscoped().Table("posts").Where("id =?", id).Find(&res).Error
+	if id == "" {
+		log.Println("error")
+	}
+	log.Println("check 2")
+	err := db.DB.Debug().Unscoped().Table("posts").Where("id =?", &id).Find(&res).Error
 	if err != nil {
 		return nil, err
 	}
 	return &res, nil
+}
+
+func Update(req *models.Post) (*models.Post, error) {
+	err := db.DB.Debug().Unscoped().Table("posts").Save(req).Error
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := GetPost(req.Id.String())
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func Delete(PostId string) error {
+
+	err := db.DB.Debug().Exec(`DELETE FROM posts WHERE id = ? `, PostId).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/services/service.go
+++ internal/services/service.go
@@ -27,25 +27,31 @@ func (s *PostServiceServer) CreatePost(ctx context.Context, req *protos.CreatePo
 
 	post := req.Post
 	log.Printf("Received request to create post: %+v", post)
-	post.PostId = uuid.New().String()
-
+	if post.PostId == uuid.Nil.String() || post.PostId == "" {
+		post.PostId = uuid.New().String()
+	}
+	log.Printf("check 1")
 	daomodel := s.DtosToDao(post)
-	err := daos.SaveCustomer(&daomodel)
+	log.Printf("check 2")
+	err := daos.SaveCustomer(daomodel)
 	if err != nil {
 		return nil, err
 	}
-
+	log.Printf("check 3")
 	return &protos.CreatePostResponse{Post: post}, nil
 }
 
 func (s *PostServiceServer) GetPost(ctx context.Context, req *protos.GetPostRequest) (*protos.GetPostResponse, error) {
 
 	postID := req.PostId
+	log.Println("check 1")
 	post, err := daos.GetPost(postID)
 	if err != nil {
 		return nil, err
 	}
+	log.Println("check 4")
 	Post := s.DaoToDtos(post)
+	log.Println("check 3")
 	return &protos.GetPostResponse{
 		Post: &Post,
 	}, nil
@@ -75,15 +81,46 @@ func (s *PostServiceServer) GetAllPosts(ctx context.Context, emp *emptypb.Empty)
 
 	}
 	allRes.Posts = Posts
-	fmt.Println("allres:", &allRes)
+	for _, val := range allRes.Posts {
+		fmt.Println(val)
+	}
+	// fmt.Println("allres:", &allRes)
 	return &allRes, nil
 }
+func (s *PostServiceServer) DeletePost(ctx context.Context, req *protos.DeletePostRequest) (*protos.DeletePostResponse, error) {
+	res := &protos.DeletePostResponse{}
+	postId := req.PostId
+	fmt.Println("adaskl")
+	err := daos.Delete(postId)
+	if err != nil {
+		res.Success = false
+		return res, err
+	}
+	res.Success = true
+	return res, nil
+}
 
-func (s *PostServiceServer) DtosToDao(req *protos.Post) models.Post {
+func (s *PostServiceServer) UpdatePost(ctx context.Context, req *protos.UpdatePostRequest) (*protos.UpdatePostResponse, error) {
+
+	Post := s.DtosToDao(req.NewPostDetails)
+	Post.Id, _ = uuid.Parse(req.PostId)
+	res, err := daos.Update(Post)
+	if err != nil {
+		return nil, err
+	}
+
+	post := s.DaoToDtos(res)
+
+	return &protos.UpdatePostResponse{
+		UpdatedPostDetails: &post,
+	}, nil
+}
+
+func (s *PostServiceServer) DtosToDao(req *protos.Post) *models.Post {
 
 	id, _ := uuid.Parse(req.PostId)
 	date := ConvertTimestamppbToTime(req.PublicationDate)
-	return models.Post{
+	return &models.Post{
 		Id:              id,
 		Title:           req.Title,
 		Content:         req.Content,
